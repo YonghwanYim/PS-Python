c09e87169b13d0484ce24f42ac6c14a215c83ec9
Yonghwan Yim <uko991531@gmail.com>
2025-04-29 15:31:57 +0900

    Create 1_Two_Sum.py

---
+++ 1_Two_Sum.py
@@ -0,0 +1,21 @@
+"""
+Author : Yonghwan Yim
+LeetCode 1. Two Sum
+https://leetcode.com/problems/two-sum/description/
+"""
+
+
+
+
+# Brute force O(N^2)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
