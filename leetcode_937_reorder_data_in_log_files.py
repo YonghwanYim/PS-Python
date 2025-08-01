ef5487382588983127df283d1e0953ca210e65ea
Yonghwan Yim <uko991531@gmail.com>
2025-08-01 12:37:52 +0900

    leetcode 937

--- leetcode_937_reorder_data_in_log_files.py
+++ leetcode_937_reorder_data_in_log_files.py
@@ -4,14 +4,31 @@
 * LeetCode 937. Reorder Data in Log Files
 * https://leetcode.com/problems/reorder-data-in-log-files/description/
 """
-
 from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []   # empty list
+        for log in logs: # letter, digit의 두 타임을 먼저 나눔
+            if log.split()[1].isdigit():  # spilt 후 두 번째 요소를 가져옴 ([1] <- 이 인덱스 의미)
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        # lambda로 간단하게 표현. identifier를 제외하고 비교. 같으면 identifier을 기준으로 정렬.
+        letters.sort(key=lambda x : (x.split()[1:], x.split()[0]))   # x.split()[0] means identifier
+        return letters + digits
+
+# Example
+logs1 = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+logs2 = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+
+# Test
+sol = Solution()
+print(sol.reorderLogFiles(logs1))
+print(sol.reorderLogFiles(logs2))
 
 
 
 
 
-        return 0
