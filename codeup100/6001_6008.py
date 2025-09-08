bc7372492c14148a83cff64f601437966488891a
Yonghwan Yim <uko991531@gmail.com>
2025-09-08 16:39:46 +0900

    CoodUp 6001 ~ 6008

---
+++ codeup100/6001_6008.py
@@ -0,0 +1,47 @@
+"""
+* Author : Yonghwan Yim
+* Final Update : 25.09.08
+
+* Title : CodeUp Python 기초 100제
+* Sub : [기초-출력] 6001 ~ 6008
+"""
+
+"""
+* 6001. 출력하기01 - 기본 출력
+"""
+print("Hello")
+
+"""
+* 6002. 출력하기02 - 공백 넣기
+"""
+print("Hello World")
+
+"""
+* 6003. 출력하기03 - 개행
+"""
+print("Hello\nWorld")
+
+"""
+* 6004. 출력하기04 - ' 출력하기
+"""
+print("'Hello'")
+
+"""
+* 6005. 출력하기05 - " 출력하기
+"""
+print("\"Hello World\"")
+
+"""
+* 6006. 출력하기06 - 특수문자 출력
+"""
+print("\"!@#$%^&*()\'")
+
+"""
+* 6007. 출력하기07 - 파일경로 출력
+"""
+print("\"C:\\Download\\\'hello\'.py\"")
+
+"""
+* 6008. 출력하기08 - print("Hello\nWorld") 출력하기
+"""
+print("print(\"Hello\\" + "nWorld\")")
